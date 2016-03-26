34546f7fd45e5b3871a8249aebdcb43d218294d2
Rohit Bhatta <rbhatta@cs.utexas.edu>
2016-03-26 00:55:42 -0500

    cr stuff

--- ppc.v
+++ ppc.v
@@ -112,9 +112,9 @@ module main();
 
     //Branching
     wire [0:63]branchTarget = allBc ? (isAA ? (pc + extendBD) : extendBD) : (allB ? ((isAA ? (pc + extendLI) : extendLI)) : extendLR);
-    wire less = (bo == 1 & bi == 0 & cr != 8) | (bo == 3 & bi == 0 & cr ==8);
-    wire greater = (bo == 1 & bi == 1 & cr != 4) | (bo == 3 & bi == 1 & cr == 4);
-    wire equals = (bo == 1 & bi == 2 & cr != 2) | (bo == 3 & bi == 1 & cr == 2);
+    wire less = (bo == 1 & bi == 0 & cr[0] != 1) | (bo == 3 & bi == 0 & cr[0] == 1);
+    wire greater = (bo == 1 & bi == 1 & cr[1] != 1) | (bo == 3 & bi == 1 & cr[1] == 1);
+    wire equals = (bo == 1 & bi == 2 & cr[2] != 1) | (bo == 3 & bi == 1 & cr[2] == 1);
     wire isBranching = allB | (allBc & (less | greater | equals)) | (allBclr & (less | greater | equals));
 
     wire updateRegs = allAdd | allOr | isAddi;
